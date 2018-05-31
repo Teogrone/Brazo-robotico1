16059fbe1f9f77320972d774150db8cf22dddcdf
juan david medina <juandavid_9619@hotmail.com>
2018-05-31 10:34:07 -0500

    Ya agarra el cubo pero con pocos vectores

--- juan.ino
+++ juan.ino
@@ -1,34 +1,41 @@
 #include <Servo.h>
+#define pinbase 13
+#define pinladod 10
+#define pinladoi 11
+#define pingarra 9
+#define NFIL 3
+
+float matriz_b [NFIL]={40,123,175};
+float matriz_d [NFIL]={40,57,60};
+float matriz_i [NFIL]={130,100,115};
+float matriz_g [NFIL]={80,185,70};
+
+Servo garra;
 Servo base;
-int pos=0;
-int TECLA=0; 
-
-
+Servo ladod;
+Servo ladoi;
+int i;
 
 void setup() {
-Serial.begin(9600);
-  base.attach(11);
-  pos=0; 
+  base.attach (pinbase);
+  ladod.attach (pinladod);
+  ladoi.attach (pinladoi);
+  garra.attach (pingarra);
+
+
+  for(i=0;i<20;i++){
+  base.write(matriz_b[i]);
+  delay(1000);
+  ladod.write(matriz_d[i]);
+  delay(1000);
+  ladoi.write(matriz_i[i]);
+  delay(1000);
+  garra.write(matriz_g[i]);
+  delay(1000);
+
+  }  
 }
 
 void loop() {
-base.write(pos);
-    delay(50);
-    Serial.println(pos);
-    if (Serial.available() > 0) { 
-       delay(100);
-       TECLA=Serial.read();
-       if (TECLA==49){  //es decir,  "1" 
-          pos=0;
-          }
-       if (TECLA==50){  //es decir,  "2" 
-          pos=90;
-          }
-       if (TECLA==51){  //es decir,  "3" 
-          pos=180;
-          }
-       }
-
-  
-  
 }
+
